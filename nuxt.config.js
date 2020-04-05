38bb5e50309b10885b8345217b740527f97e3e22
David, Shin <dirmich@gmail.com>
2020-04-05 21:02:05 +0900

    a

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,35 +1,45 @@
 const routerBase =
-  process.env.USE_SUBFOLDER === "true"
-    ? {
-        router: {
-          base: "/nuxt-tensorflow/",
-        },
-      }
-    : {};
+  process.env.USE_SUBFOLDER === "true" ?
+  {
+    router: {
+      base: "/nuxt-tensorflow/",
+    },
+  } :
+  {};
 
 export default {
   ...routerBase,
-  mode: "universal",
+  mode: "spa",
   /*
    ** Headers of the page
    */
   head: {
     title: process.env.npm_package_name || "",
-    meta: [
-      { charset: "utf-8" },
-      { name: "viewport", content: "width=device-width, initial-scale=1" },
+    meta: [{
+        charset: "utf-8"
+      },
+      {
+        name: "viewport",
+        content: "width=device-width, initial-scale=1"
+      },
       {
         hid: "description",
         name: "description",
         content: process.env.npm_package_description || "",
       },
     ],
-    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+    link: [{
+      rel: "icon",
+      type: "image/x-icon",
+      href: "/favicon.ico"
+    }],
   },
   /*
    ** Customize the progress-bar color
    */
-  loading: { color: "#fff" },
+  loading: {
+    color: "#fff"
+  },
   /*
    ** Global CSS
    */
